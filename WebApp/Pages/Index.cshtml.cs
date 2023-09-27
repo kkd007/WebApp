266f28aa7f3e284cbae9c9590eeb3bcd064651c1
SAI_LG\kiran <mail_kkd@yahoo.com>
2023-09-27 15:34:01 -0500

    Add project files.

---
+++ WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+using WebApp.Services;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+        }
+    }
+}
